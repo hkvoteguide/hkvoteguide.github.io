19daa1963f4b684e9605d335da7500d4bce172b5
Nandiheath <nandi0315@gmail.com>
2019-08-31 13:42:45 +0800

    Updates

---
+++ precache-manifest.3b35425add28c510b37dee74d79750b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32a2c9fb4360314b4ba9",
+    "url": "/district-councils-dashboard/static/js/runtime~main.8cf94a55.js"
+  },
+  {
+    "revision": "cce7a50249bb387654ab",
+    "url": "/district-councils-dashboard/static/js/main.170d8d90.chunk.js"
+  },
+  {
+    "revision": "36573ef5709b190188d7",
+    "url": "/district-councils-dashboard/static/js/2.574c074f.chunk.js"
+  },
+  {
+    "revision": "cce7a50249bb387654ab",
+    "url": "/district-councils-dashboard/static/css/main.70f29447.chunk.css"
+  },
+  {
+    "revision": "6a2ba8ab5202d9d950a34112a4d77a84",
+    "url": "/district-councils-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4c0208956fdc0f4852c9fca3654c576.js"
+  "/district-councils-dashboard/precache-manifest.3b35425add28c510b37dee74d79750b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/district-councils-dashboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
