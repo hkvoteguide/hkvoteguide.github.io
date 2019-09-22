b36f54218a5ff7fdedc5563ae833520bdf8b389c
Nandiheath <nandi0315@gmail.com>
2019-09-22 15:20:12 +0800

    Updates

---
+++ precache-manifest.51f98f4d612806425a1991a29abaf5d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2411a2042149a03e33d1",
+    "url": "/static/js/main.0518497a.chunk.js"
+  },
+  {
+    "revision": "5cc60748da42f56a5a2f",
+    "url": "/static/js/2.d52f0749.chunk.js"
+  },
+  {
+    "revision": "2411a2042149a03e33d1",
+    "url": "/static/css/main.70f29447.chunk.css"
+  },
+  {
+    "revision": "3067f26929431916bdc4ea78db0ac027",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.650793d4f61e016db31b71388c1b2978.js"
+  "/precache-manifest.51f98f4d612806425a1991a29abaf5d6.js"
 );
 
 workbox.clientsClaim();
