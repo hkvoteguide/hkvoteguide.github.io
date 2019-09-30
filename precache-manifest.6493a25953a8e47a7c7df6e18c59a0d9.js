cb770e863245a50c3520afed7972be469510b02b
Brian Leung <brian_yf_leung@manulifeam.com>
2019-10-01 01:28:32 +0800

    Updates

---
+++ precache-manifest.6493a25953a8e47a7c7df6e18c59a0d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4fa4985b4c5518c36392",
+    "url": "/static/js/main.df0b5fcc.chunk.js"
+  },
+  {
+    "revision": "2a6e32409e5f759b99a9",
+    "url": "/static/js/2.5f80a413.chunk.js"
+  },
+  {
+    "revision": "4fa4985b4c5518c36392",
+    "url": "/static/css/main.25af8312.chunk.css"
+  },
+  {
+    "revision": "595b120b4327f2a8fb6b15208e244243",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.197fe3ca808fed0fb69483554dd7eb45.js"
+  "/precache-manifest.6493a25953a8e47a7c7df6e18c59a0d9.js"
 );
 
 workbox.clientsClaim();
