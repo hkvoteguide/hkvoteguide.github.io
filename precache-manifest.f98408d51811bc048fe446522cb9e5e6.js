f758dc1a61819a2b92ca58a140cc5bd860f15af9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-19 15:38:40 +0000

    Deploy dc2019.g0vhk.io to github.com/hkvoteguide/hkvoteguide.github.io.git:master

---
+++ precache-manifest.f98408d51811bc048fe446522cb9e5e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8d8eaf7d43326345c100",
+    "url": "/static/js/main.a6195f59.chunk.js"
+  },
+  {
+    "revision": "2ce9abef30b198e74ce5",
+    "url": "/static/js/2.4ca03b09.chunk.js"
+  },
+  {
+    "revision": "8d8eaf7d43326345c100",
+    "url": "/static/css/main.20612af9.chunk.css"
+  },
+  {
+    "revision": "467af196c4385ba460adbcef428ed73b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://dc2019.g0vhk.io/precache-manifest.4a5b223c3b5dc25a4fbb17767b65b3e9.js"
+  "/precache-manifest.f98408d51811bc048fe446522cb9e5e6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://dc2019.g0vhk.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
