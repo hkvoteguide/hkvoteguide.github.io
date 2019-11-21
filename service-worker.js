2abdeea995d9fc482db392fac741718a3343626f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-21 04:43:55 +0000

    Deploy vote4.hk to github.com/hkvoteguide/hkvoteguide.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://vote4.hk/precache-manifest.7c8f722e0c88e27ea2104690d5487b83.js"
+  "https://vote4.hk/precache-manifest.53c16163935787d684292d79ace00998.js"
 );
 
 workbox.clientsClaim();
