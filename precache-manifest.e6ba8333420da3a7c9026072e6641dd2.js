316db3a4984869bc78cc8ff3bcf16e35fd24cf32
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-24 04:11:32 +0000

    Deploy vote4.hk to github.com/hkvoteguide/hkvoteguide.github.io.git:master

---
+++ precache-manifest.e6ba8333420da3a7c9026072e6641dd2.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "65f3e1915cc1fcbabde1",
+    "url": "https://vote4.hk/static/js/16.22f8a9d0.chunk.js"
+  },
+  {
+    "revision": "593423f0460933246cd9",
+    "url": "https://vote4.hk/static/js/0.9f762287.chunk.js"
+  },
+  {
+    "revision": "3584e19170fbc141f3bd",
+    "url": "https://vote4.hk/static/js/2.8716bd05.chunk.js"
+  },
+  {
+    "revision": "302272e30f9bf63a4102",
+    "url": "https://vote4.hk/static/js/3.aa6173fd.chunk.js"
+  },
+  {
+    "revision": "a4e553a5cb88ce60a0ef",
+    "url": "https://vote4.hk/static/js/4.b61d4a54.chunk.js"
+  },
+  {
+    "revision": "7908b771780ce7c3adfc",
+    "url": "https://vote4.hk/static/js/5.d03f76e7.chunk.js"
+  },
+  {
+    "revision": "f7522edb28cbbadbe6e4",
+    "url": "https://vote4.hk/static/js/6.dc68a782.chunk.js"
+  },
+  {
+    "revision": "6bce714dbfe34b531f8f",
+    "url": "https://vote4.hk/static/js/31.2b6dfdbc.chunk.js"
+  },
+  {
+    "revision": "d09c4b1596b7bd7d595a",
+    "url": "https://vote4.hk/static/js/main.cbb57afb.chunk.js"
+  },
+  {
+    "revision": "7be0c9fb48f1b2785ce8",
+    "url": "https://vote4.hk/static/js/runtime~main.d7485aad.js"
+  },
+  {
+    "revision": "80b1e3da8707eb6ef6f3",
+    "url": "https://vote4.hk/static/js/9.9f0d62b1.chunk.js"
+  },
+  {
+    "revision": "5a96b040a44a2a611dd1",
+    "url": "https://vote4.hk/static/js/10.6c0d13a8.chunk.js"
+  },
+  {
+    "revision": "d634b83e19eae639889c",
+    "url": "https://vote4.hk/static/js/11.bc63f992.chunk.js"
+  },
+  {
+    "revision": "336de361034a85de10b5",
+    "url": "https://vote4.hk/static/js/12.b2ee6edf.chunk.js"
+  },
+  {
+    "revision": "a4f837093857405a905c",
+    "url": "https://vote4.hk/static/js/13.83023f4f.chunk.js"
+  },
+  {
+    "revision": "f021cab9d0316aa74567",
+    "url": "https://vote4.hk/static/js/14.76777c82.chunk.js"
+  },
+  {
+    "revision": "4ab356881fce8e419d68",
+    "url": "https://vote4.hk/static/js/15.1d66212f.chunk.js"
+  },
+  {
+    "revision": "7aabb5dd2946dedc6203",
+    "url": "https://vote4.hk/static/js/1.f43cd5ad.chunk.js"
+  },
+  {
+    "revision": "6dca90102adbeb6b77dd",
+    "url": "https://vote4.hk/static/js/17.97ab1533.chunk.js"
+  },
+  {
+    "revision": "dbb510f274256ee7b142",
+    "url": "https://vote4.hk/static/js/18.7fbcdca8.chunk.js"
+  },
+  {
+    "revision": "8c1b8a1a82256ef6e2d0",
+    "url": "https://vote4.hk/static/js/19.aedb4f49.chunk.js"
+  },
+  {
+    "revision": "0326c51db8a833c449a0",
+    "url": "https://vote4.hk/static/js/20.34ecf5af.chunk.js"
+  },
+  {
+    "revision": "06f2e1e77ae63ab2631c",
+    "url": "https://vote4.hk/static/js/21.01b5fad6.chunk.js"
+  },
+  {
+    "revision": "b48143f877b0fa097f7f",
+    "url": "https://vote4.hk/static/js/22.bd55112d.chunk.js"
+  },
+  {
+    "revision": "eabc4ea486ae34b2df4e",
+    "url": "https://vote4.hk/static/js/23.5f5d348d.chunk.js"
+  },
+  {
+    "revision": "377aea17969fc1ffbcf1",
+    "url": "https://vote4.hk/static/js/24.2a7f4217.chunk.js"
+  },
+  {
+    "revision": "bd05b13e1e575a37f54d",
+    "url": "https://vote4.hk/static/js/25.d33d0aef.chunk.js"
+  },
+  {
+    "revision": "ac056b091f94685389d7",
+    "url": "https://vote4.hk/static/js/26.8aed0fe8.chunk.js"
+  },
+  {
+    "revision": "ea8d5c6d150f06bc4d4a",
+    "url": "https://vote4.hk/static/js/27.8e18efd3.chunk.js"
+  },
+  {
+    "revision": "73c8223db9a264f910a8",
+    "url": "https://vote4.hk/static/js/28.a300d7e9.chunk.js"
+  },
+  {
+    "revision": "a4c46bfcb7203966d3c0",
+    "url": "https://vote4.hk/static/js/29.9005b817.chunk.js"
+  },
+  {
+    "revision": "750bddc77418de9a59ef",
+    "url": "https://vote4.hk/static/js/30.e0bcb5ba.chunk.js"
+  },
+  {
+    "revision": "d09c4b1596b7bd7d595a",
+    "url": "https://vote4.hk/static/css/main.0cab265a.chunk.css"
+  },
+  {
+    "revision": "a2f6bb72875c122fcf2b0b1e876ffc28",
+    "url": "https://vote4.hk/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://vote4.hk/precache-manifest.32cd9a728158aae31f35f9dfba1098eb.js"
+  "https://vote4.hk/precache-manifest.e6ba8333420da3a7c9026072e6641dd2.js"
 );
 
 workbox.clientsClaim();
